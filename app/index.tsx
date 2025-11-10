b887c56613b6574820afa163db5226a9bf169827
G-Baudelaire <germainjones1@gmail.com>
2025-11-10 00:36:51 +0000

    Basic audio player setup.

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,37 @@
 import React from "react";
-import {StyleSheet, Text, View} from 'react-native';
-import TrackPlayer from "react-native-track-player";
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {useAudioPlayer, useAudioPlayerStatus} from 'expo-audio';
+import Pearls from "@/assets/Pearls.mp3"
+
+const audioSource = Pearls;
 
 export default function App() {
+    const player = useAudioPlayer(audioSource);
+    const status = useAudioPlayerStatus(player);
+
     return (
         <View style={styles.container}>
-            <Text>Hello World</Text>
+            <Button title="Play Sound" onPress={() => player.play()}/>
+            <Button
+                title="Replay Sound"
+                onPress={() => {
+                    player.seekTo(0);
+                    player.play();
+                    console.log("button press");
+                }}
+            />
+            <Text>
+                {!status?.isLoaded ? "Loading…" : status.isBuffering ? "Buffering…" : status.playing ? "Playing" : "Paused"}
+            </Text>
         </View>
     );
 }
 
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
         justifyContent: 'center',
+        backgroundColor: '#ecf0f1',
+        padding: 10,
     },
 });
